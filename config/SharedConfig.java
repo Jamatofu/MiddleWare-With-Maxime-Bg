893a35dff766dfc5c297b0cc2b3e361e61e14d58
maxim <maxime.flament@etu.unice.fr>
2018-04-18 10:10:54 +0200

    config

---
+++ config/SharedConfig.java
@@ -0,0 +1,9 @@
+/**
+ * @author Maxime Flament (maxime.flament@etu.unice.fr)
+ */
+public class SharedConfig {
+
+    private static final String HOST = "http://localhost:";
+    private static final String PORT = "8080";
+    public static final String COMPLETE_URL = HOST + PORT;
+}
--- consumer/ConsumerClient.java
+++ consumer/ConsumerClient.java
@@ -1,5 +1,36 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author Maxime Flament (maxime.flament@etu.unice.fr)
  */
 public class ConsumerClient {
+
+    private static final Logger LOGGER = Logger.getLogger(ConsumerClient.class.getName());
+
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            LOGGER.log(Level.SEVERE, "This program takes one argument that allows to identify the consumer!");
+            System.exit(-1);
+        }
+
+        LOGGER.log(Level.INFO, "Starting consumer client...");
+
+        ProducerInterface producerInterface = null;
+        Registry registry;
+
+        try {
+            producerInterface = (ProducerInterface) Naming.lookup("//" + args[0] + "/ProducerInterface");
+        } catch (RemoteException | NotBoundException | MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
