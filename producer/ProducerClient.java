1f9fa1bfb17f6d537be2831962f8f50d3bb101fa
Jamatofu <clement.beal96@gmail.com>
2018-04-18 10:04:30 +0200

    Producer

--- producer/ProducerClient.java
+++ producer/ProducerClient.java
@@ -1,5 +1,30 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * @author Maxime Flament (maxime.flament@etu.unice.fr)
  */
-public class ProducerClient {
+public class ProducerClient extends UnicastRemoteObject implements ProducerInterface{
+    private String url;
+
+    protected ProducerClient() throws RemoteException {
+    }
+
+    @Override
+    public void getUrl(String url) throws RemoteException {
+        System.out.println("Url reçu : " + url);
+        this.url = url;
+    }
+
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099);
+            ProducerClient client = new ProducerClient();
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
